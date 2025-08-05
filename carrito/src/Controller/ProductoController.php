ab1e2ff47ed307c81a5692954f688ed71766f029
maariasGH <capo51240@gmail.com>
2025-08-05 18:58:42 -0300

    Base de Datos y Doctrine

---
+++ carrito/src/Controller/ProductoController.php
@@ -0,0 +1,18 @@
+<?php
+    namespace App\Controller;
+
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
+
+    class ProductoController extends AbstractController {
+
+        #[Route( '/', name: 'listar_productos')] 
+
+        public function listarProductos() : Response {
+            $html = '<html><body><b>Esta es la lista de productos</b></body></html>';
+            return $this->render("producto/lista.html.twig");
+        }
+    } 
+
+?>
