7e7c5f460ad0d7f701a7ea5e27e52c5a0ae25f93
maariasGH <capo51240@gmail.com>
2025-08-12 18:51:10 -0300

    Integrando Twig con Composer yla Base de Datos

--- carrito/src/Controller/ProductoController.php
+++ carrito/src/Controller/ProductoController.php
@@ -4,14 +4,16 @@
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
+    use App\Repository\ProductoRepository;
 
     class ProductoController extends AbstractController {
 
         #[Route( '/', name: 'listar_productos')] 
 
-        public function listarProductos() : Response {
+        public function listarProductos(ProductoRepository $repository) : Response {
             $html = '<html><body><b>Esta es la lista de productos</b></body></html>';
-            return $this->render("producto/lista.html.twig");
+            $productos = $repository->findAll(); 
+            return $this->render("producto/lista.html.twig",  ['productos' => $productos]);
         }
     } 
 
